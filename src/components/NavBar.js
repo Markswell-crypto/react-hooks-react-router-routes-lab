647e1526b8acad6300ced41b68439689e00ad6f9
Markswell <osoromarkswell80@gmail.com>
2023-10-31 17:39:46 +0300

    Still Working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <NavLink to="/">HOME</NavLink>
+    <NavLink to="/movies">MOVIES</NavLink>
+    <NavLink to="/actors">ACTORS</NavLink>
+    <NavLink to="/directors">DIRECTORS</NavLink>
+  </div>
+    );
 }
 
 export default NavBar;
